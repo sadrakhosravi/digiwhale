ca78f9d1de776bcd07878f8293a9196e80c55093
Sadra Khosravi <sadrakh@outlook.com>
2021-03-17 23:06:32 -0700

    Add website header and hero section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const header = document.querySelector('header');
+
+//On scroll add header-scroll class to header
+document.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('header-scroll');
+  } else {
+    header.classList.remove('header-scroll');
+  }
+});
